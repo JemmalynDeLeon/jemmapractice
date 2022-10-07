9e0ecbdf61225faaa219d90bfb1703f96e7b7b2e
JemmalynDeLeon <jemmaadeleon@gmail.com>
2022-10-07 15:03:27 +0800

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainSwipeActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import me.relex.circleindicator.CircleIndicator3
+
+
+
+class MainSwipeActivity : AppCompatActivity() {
+
+    private var titleList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_swipe)
+
+        postToList()
+
+        view_pager2.adapter = ViewPagerAdapter(titleList,descList,imagesList)
+        view_pager2.orietation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        val indicator = findViewById<CircleIndicator3>(R.id.indicator)
+        indicator.setViewPager(view_pager2)
+    }
+
+    private fun addToList(title: String, description: String, image: Int) {
+        titleList.add(title)
+        descList.add(description)
+        imagesList.add(image)
+
+
+    }
+    private fun postToList(){
+        for (i in 1..5) {
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher_round)
+        }
+
+    }
+
+}
